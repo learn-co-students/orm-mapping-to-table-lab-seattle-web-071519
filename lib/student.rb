e046e6bdb2ab3aeec70c6fb7a1d1fc37068d5a18
ayerest <irisiseve@gmail.com>
2019-07-25 13:40:38 -0700

    finished orm mapping to table lab

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,49 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  #class method because the individual instances are 
+  #not responsible for setting up the table
+  def self.create_table 
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?);
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    #row below is how we update the id attribute to match the id in the table
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name: nname, grade: grade)
+    new_student = Student.new(name, grade)
+    new_student.save
+    new_student
+  end
   
 end
